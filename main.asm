46157bfeb72ed538c9c478ee10c296046988caff
Master60 <habebelrhman321@gmail.com>
2023-01-01 20:07:41 +0200

    Main Chat

--- main.asm
+++ main.asm
@@ -63,6 +63,12 @@
 
     dummy2                   db      'Press F3 to exit', '$'
 
+    IY DB 0D
+IX DB 0D
+OX DB 39D
+OY DB 0D
+VLine db '#'
+
     ;---------------------------------------------------------------------------------------------------------------------------------------------
     ;VARIABLES USED FOR PREPARING AND DRAWING THE BOARD:
     ;---------------------------------------------------------------------------------------------------------------------------------------------
@@ -1408,7 +1414,203 @@ draw_numbers proc
 
 draw_numbers endp
 
+intializePort proc
+mov dx,3fbh ;line control register
+mov al,10000000b ;set divisor latch access bit
+out dx,al ;out it
 
+mov dx,3f8h 
+mov al,0ch
+out dx,al
+
+mov dx,3f9h
+mov al,00h
+out dx,al
+
+mov dx,3fbh
+mov al,00011011b
+out dx,al
+
+ret
+intializePort endp
+
+WRITEINPUT PROC
+cmp al,13d
+jne cont1
+cmp Iy,24d
+jb cont1
+cmp ix,37d
+jb cc
+CALL newILine
+mov AH,2
+mov DL,IX
+MOV DH,IY
+int 10h
+call clearInputScreen
+RET
+cc:
+call clearInputScreen
+call newILine
+mov IY,24d
+mov AH,2
+mov DL,IX
+MOV DH,IY
+int 10h
+ret
+cont1:
+CMP AL,13d
+JE IENTER
+CMP ix,37
+jb p1
+mov IX,0
+inc IY
+p1:
+mov AH,2
+mov DL,IX
+MOV DH,IY
+int 10h
+
+mov ah,2
+mov dl,AL
+int 21h
+INC IX
+RET
+IENTER:
+CALL newILine
+mov AH,2
+mov DL,IX
+MOV DH,IY
+int 10h
+RET
+WRITEINPUT ENDP
+
+
+WRITEOUTPUT PROC
+cmp al,13d
+jne cont2
+cmp oy,24d
+jb cont2
+cmp ox,79d
+jb cc1
+call clearOutputScreen
+RET
+cc1:
+call clearOutputScreen
+call newOLine
+mov oY,24d
+mov AH,2
+mov DL,oX
+MOV DH,oY
+int 10h
+ret
+cont2:
+CMP AL,13d
+JE OENTER
+CMP ox,79
+jb p2
+mov oX,39
+inc oY
+p2:
+mov AH,2
+mov DL,oX
+MOV DH,oy
+int 10h
+
+mov ah,2
+mov dl,AL
+int 21h
+INC oX
+RET
+OENTER:
+CALL newOLine
+mov AH,2
+mov DL,OX
+MOV DH,OY
+int 10h
+RET
+WRITEOUTPUT ENDP
+
+SENDKEY PROC
+MOV DX,3F8H
+OUT DX,AL
+RET
+SENDKEY ENDP
+
+inializeScreen proc
+mov ah,0
+mov al,3h
+int 10h
+
+mov ah,2
+mov dl,38
+mov dh,0
+mov cx,25
+lp:
+	mov ah,2
+	int 10h
+	mov dl,'#'
+	int 21h
+	mov dl,38
+	inc dh
+LOOP lp
+
+
+ret
+inializeScreen endp
+
+
+clearInputScreen proc
+	pusha
+	mov al,1h     ; function 6
+	mov ah,6h
+   mov bh,07h       ; normal video attribute         
+   mov ch,0       ; upper left Y
+   mov cl,0        ; upper left X
+   mov dh,24    ; lower right Y
+   mov dl,37      ; lower right X 
+   int 10h  
+mov ah,3
+mov bh,0
+int 10h   
+mov ah,2
+mov dl,' '
+int 21h
+popa
+ret
+clearInputScreen endp
+
+
+clearOutputScreen proc
+pusha
+	mov al,1h     ; function 6
+	mov ah,6h
+   mov bh,07h       ; normal video attribute         
+   mov ch,0       ; upper left Y
+   mov cl,39        ; upper left X
+   mov dh,24    ; lower right Y
+   mov dl,79      ; lower right X 
+   int 10h  
+mov ah,3
+mov bh,0
+int 10h   
+mov ah,2
+mov dl,' '
+int 21h
+popa
+ret
+clearOutputScreen endp
+
+newILine proc
+mov IX,0
+inc IY
+ret
+newILine endp
+
+newOLine proc
+mov OX,39
+inc OY
+ret
+newOLine endp
 
     ;---------------------------------------------------------------------------------------------------------------------------------------------
     ;PROCEDURES USED IN THE CHAT SCREEN:
@@ -1502,6 +1704,58 @@ chat_window proc
                                                 ret
 
 chat_window endp
+
+chat_window_2 proc
+
+pusha
+
+call inializeScreen
+	CALL intializePort
+	
+	;CODE
+	CHECKKEYPRESSED:
+	
+	;CHECK IF THERE IS A KEY PRESSESD SEND TO THE OTHER USER
+	MOV AH,01h
+	INT 16H
+	JZ CHECKKEYSENT
+	MOV AH,00
+	INT 16H
+	CMP AL,1BH
+	JE EXIT
+	call WRITEINPUT
+	CALL SENDKEY
+	
+
+	CHECKKEYSENT:
+	;CHECK STATE IF THERE IS DATA RECIVED
+	;IF THERE IS NO DATA RECIVED
+	MOV DX,3FDH
+	IN AL,DX
+	AND AL,1
+	JZ CHECKKEYPRESSED
+	;IF THERE IS DATA RECIVED
+	;RECIVE DATA AND CALL WRITE IN OUTPUT PROC
+	MOV DX,03F8H
+	IN AL,DX
+	CALL WRITEOUTPUT
+
+	JMP CHECKKEYPRESSED
+	;END CODE
+	
+	
+             
+                 
+                 
+     EXIT:
+	 call clearInputScreen
+	 call clearOutputScreen
+
+popa
+
+ret
+
+chat_window_2 endp
 
     ;---------------------------------------------------------------------------------------------------------------------------------------------
     ;PROCEDURES USED IN DRAWING ON THE GAME SCREEN:
@@ -5505,7 +5759,7 @@ main_window proc
                                                 jmp   main_end
 
     start_chat:                                 
-                                                call  chat_window
+                                                call  chat_window_2
                                                 jmp   main_start
 
     start_game:                                 
